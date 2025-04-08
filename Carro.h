edd6861124cfed989d49808ffe654d87555741c0
José Cláudio <jvalentino9.2005@gmail.com>
2025-04-08 12:49:10 +0100

    definir as estruturas

--- Carro.h
+++ Carro.h
@@ -1,6 +1,29 @@
 #ifndef CARRO_H_INCLUDED
 #define CARRO_H_INCLUDED
 
+#define MAX_MATRICULA 10
+#define MAX_MODELOS 50
+#define MAX_MARCAS 50
 
+typedef struct {
+    char matricula[MAX_MATRICULA];
+    char modelo[MAX_MODELOS];
+    char marca[MAX_MARCAS];
+    int ano;
+    int donoNIF;
+    int codVeiculo;
+    int kmTotal;
+} Carro;
+
+typedef struct nodoCarro {
+    Carro carro;
+    struct nodoCarro* seguinte;
+} NodoCarro;
+
+// Funções
+NodoCarro* carregarCarros(const char* nomeFicheiro);
+NodoCarro* adicionarCarro(NodoCarro* lista, Carro novo);
+void listarCarros(NodoCarro* lista);
+void libertarCarros(NodoCarro* lista);
 
 #endif // CARRO_H_INCLUDED
--- Dono.h
+++ Dono.h
@@ -1,6 +1,25 @@
 #ifndef DONO_H_INCLUDED
 #define DONO_H_INCLUDED
 
+#define MAX_NOME 100
+#define MAX_COD_POSTAL 10
 
+typedef struct {
+    int numContribuinte;
+    char nome[MAX_NOME];
+    char codPostal[MAX_COD_POSTAL];
+} Dono;
+
+typedef struct nodoDono {
+    Dono dono;
+    struct nodoDono* seguinte;
+} NodoDono;
+
+// Funções
+NodoDono* carregarDonos(const char* nomeFicheiro);
+void guardarDonosCSV(NodoDono* lista, const char* nomeFicheiro);
+void listarDonos(NodoDono* lista);
+NodoDono* adicionarDono(NodoDono* lista, Dono novo);
+void libertarDonos(NodoDono* lista);
 
 #endif // DONO_H_INCLUDED
